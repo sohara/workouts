ae6e03c6b83092aac7bfa3ef029cc3c47be5f0a9
Sean O'Hara <sohara@sohara.com>
2022-01-08 16:35:30 -0500

    Start adding react-navigation basics

--- App.tsx
+++ App.tsx
@@ -1,16 +1,110 @@
 import Amplify, { Auth } from 'aws-amplify';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native';
+import {
+  NavigationContainer,
+  useNavigationState,
+} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
 import awsconfig from './src/aws-exports';
-Amplify.configure(awsconfig);
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: { itemID: number; otherParam: 'foo' | 'bar' };
+};
+
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+function HomeScreen({ navigation, route }: HomeScreenProps) {
+  // console.warn({ route });
+  // const state = useNavigationState((state) => state);
+  // console.warn({ state });
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() =>
+          navigation.navigate('Details', {
+            itemID: 86,
+            otherParam: 'foo',
+          })
+        }
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+function Details({ navigation, route }: DetailsScreenProps) {
+  const { itemID, otherParam } = route.params;
+  return (
+    <View style={styles.container}>
+      <Text>Details</Text>
+      <Text>itemID: {JSON.stringify(itemID)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.navigate('Details', {
+            itemID: 86,
+            otherParam: 'foo',
+          })
+        }
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#f4511e' },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <RootStack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        ></RootStack.Screen>
+        <RootStack.Screen
+          name="Details"
+          component={Details}
+          // options={({ route }) => ({ title: route.params.itemID })}
+          options={(options) => {
+            console.warn({ itemID: options.route.params.itemID });
+            return { title: `item ${options.route.params.itemID}` };
+          }}
+        ></RootStack.Screen>
+      </RootStack.Navigator>
+      {/* {() => {
+        return (
+          <View>
+            <Text>Hello</Text>
+          </View>
+        );
+      }} */}
+    </NavigationContainer>
   );
 }
 
